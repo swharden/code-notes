43d4e7ed2a91d009be23662747f91768e821d612
Scott W Harden <swharden@gmail.com>
2019-06-08 13:13:44 -0400

    added lots of icons

--- Csharp/icons-vs2017/displayUnlisted.py
+++ Csharp/icons-vs2017/displayUnlisted.py
@@ -4,7 +4,6 @@ with open("readme.md") as f:
     readme = f.read()
 icons = glob.glob("./*.png")
 icons = [os.path.basename(x) for x in icons]
-print "\n### Icons not seen in readme ###\n"
 for icon in icons:
     if not icon in readme:
-        print "![](%s) %s\\"%(icon, icon)
+        print("![](%s) %s\\"%(icon, icon))
