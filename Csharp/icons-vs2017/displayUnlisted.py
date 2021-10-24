8ef14c9f75711799dd8bbcf14d494ffb44409c07
Scott W Harden <swharden@gmail.com>
2021-10-24 18:13:56 -0400

    delete vs2017 icons

--- Csharp/icons-vs2017/displayUnlisted.py
+++
@@ -1,9 +0,0 @@
-import os
-import glob
-with open("readme.md") as f:
-    readme = f.read()
-icons = glob.glob("./*.png")
-icons = [os.path.basename(x) for x in icons]
-for icon in icons:
-    if not icon in readme:
-        print("![](%s) %s\\"%(icon, icon))
