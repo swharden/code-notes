53e3716c1013d00403f68913c5311ca9890b7ea3
Scott W Harden <swharden@gmail.com>
2022-02-03 23:40:41 -0500

    benchmark generic math

--- Csharp/misc/projects/BenchmarkGenericMath/Program.cs
+++ Csharp/misc/projects/BenchmarkGenericMath/Program.cs
@@ -1,16 +1,45 @@
-﻿namespace BenchmarkGenericMath;
+﻿/*
+ * RESULTS:
+
+    Generating 10,000,000 random numbers...
+
+    Testing SumDoubleArray 100 times...
+    Mean of 10,224.428 µs per sum
+
+    Testing SumGenericToDouble 100 times...
+    Mean of 9,943.770 µs per sum
+
+    Testing SumGenericMath 100 times...
+    Mean of 9,894.799 µs per sum
+
+*/
+
+namespace BenchmarkGenericMath;
 
 public static class Program
 {
+    const int POINT_COUNT = 10_000_000;
+    const int REPS_PER_FUNCTION = 100;
+    readonly static Random Rand = new(0);
+
     public static void Main()
     {
-        const int POINT_COUNT = 100_000;
-        Random rand = new(0);
-        double[] values = Enumerable.Range(0, POINT_COUNT).Select(x => rand.NextDouble()).ToArray();
+        Console.WriteLine($"Generating {POINT_COUNT:N0} random numbers...");
+        double[] values = Enumerable.Range(0, POINT_COUNT).Select(x => Rand.NextDouble()).ToArray();
+        System.Diagnostics.Stopwatch sw = new();
 
-        Console.WriteLine(SumDoubleArray(values));
-        Console.WriteLine(SumGenericToDouble(values));
-        Console.WriteLine(SumGenericMath(values));
+        Func<double[], double>[] testFunctions = { SumDoubleArray, SumGenericToDouble, SumGenericMath };
+
+        foreach (var func in testFunctions)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Testing {func.Method.Name} {REPS_PER_FUNCTION:N0} times...");
+            sw.Restart();
+            for (int i = 0; i < REPS_PER_FUNCTION; i++)
+                SumDoubleArray(values);
+            sw.Stop();
+            Console.WriteLine($"Mean of {sw.Elapsed.TotalMilliseconds / REPS_PER_FUNCTION * 1000:N3} µs per sum");
+        }
     }
 
     public static double SumDoubleArray(double[] values)
